1c51ccdacccbb32b34e89670478c12147892e6a8
Digitalboss63 <digitalpadcreatives@gmail.com>
2026-09-03 14:55:19 -0400

    Ensure media fixes load after theme styles

--- header.php
+++ header.php
@@ -3,8 +3,8 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() . '/assets/dist/media-fixes.css?v=1.3.1' ); ?>">
   <?php wp_head(); ?>
+  <link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() . '/assets/dist/media-fixes.css?v=1.3.1' ); ?>">
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
